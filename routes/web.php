f1489f7ca06ead505073ecaae74ab70e77f3ecf2
fotofacil <fotofacil@live.com>
2020-05-08 19:02:45 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('users/{id}', 'PropertiesController@param');
+
+Route::resource('properties', 'PropertiesController');
+
+// Route::fallback(function(){
+//     // echo "<h1>Deu ruim no fallback</h1>";
+//    return redirect()->route('properties.index') ;
+// });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
